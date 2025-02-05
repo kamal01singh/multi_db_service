dcdd45d779a60fe551056dceebb903b6a6f9fab3
Kamal Singh K <106004575+kamal01singh@users.noreply.github.com>
2025-02-05 21:44:40 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,4 @@
-# config.py
-
-# MongoDB Configuration
 MONGO_URI = "mongodb://localhost:27017/mydatabase"
 
-# PostgreSQL Configuration
 POSTGRES_URI = "postgresql://postgres:988625@localhost:5432/mydatabase"
 
